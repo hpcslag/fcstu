4325201485783a84714014f5168c8db5c42e39b8
unknown <cslag@hotmail.com.tw>
2015-01-06 01:11:29 +0800

    zzz

--- router/router.js
+++ router/router.js
@@ -1,6 +1,19 @@
-var md5 = require('../lib/hash');
+var url = require('url'),
+	md5 = require('../lib/hash');
+
 exports.index = function(req,res){
-	if(1==1){
+	/** found get method*/
+	var url_parts = url.parse(req.url, true),
+		query = url_parts.query; //query username if find
+
+	/** if want clean user cookie, clean*/
+	if(query.clean == "true"){
+		console.log("clean");
+		res.clearCookie('username');
+		res.redirect('/');
+	}
+	//如果有cookie才找username，不是本人就刪除cookie即可,資訊全部撈cookie的資料
+	if(1==1/*!!req.cookies.username*/){
 		//if is logined
 		res.render('singin',{logined:true,name:"Mac",gravatar:'http://www.gravatar.com/avatar/'+md5("cslag@hotmail.com.tw")+'?s=200',username:"hpcslag"});
 	}else{
@@ -20,7 +33,7 @@ function checkLogin(userName,passWord){
 			if(data.password === password){
 				logined!
 			}else{
-				res.redict('/index?error=1');
+				res.redict('/?error=1');
 			}
 		})
 		*/
