63cb123ebc7d57f80eb706a87b21c5e555fbda0b
Mac Taylor <cslag@hotmail.com.tw>
2015-01-08 12:36:28 +0000

    big update

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express'),
 	ejs = require('ejs'),
 	router = require('./router');
 
-app.configure(function(){
-	app.use(express.static(__dirname+'/www'));
+app.configure(function() {
+	app.use(express.static(__dirname + '/www'));
 	app.use(express.cookieParser());
-	app.use(express.session({secret:'MD5HeapMap'}));
+	app.use(express.session({
+		secret: 'MD5HeapMap'
+	}));
 	/**
 	*Long Alive Server session
 	
@@ -15,22 +17,50 @@ app.configure(function(){
 		secret:'https://www.youtube.com/watch?v=SR6iYWJxHqs'
 	}));*/
 	app.use(express.bodyParser()); //open GER,POST body parser
-	app.set('views',__dirname+'/www'); //views engine path
-	app.set('view engine','ejs');
-	app.use(app.router);//open routers
+	app.set('views', __dirname + '/www'); //views engine path
+	app.set('view engine', 'ejs');
+	app.use(app.router); //open routers
+
+	//404
+	app.use(function(req, res, next) {
+		res.status(404);
+
+		// respond with html page
+		if (req.accepts('html')) {
+			res.render('404');
+			return;
+		}
+
+		// respond with json
+		if (req.accepts('json')) {
+			res.send({
+				error: 'Not found'
+			});
+			return;
+		}
+
+		// default to plain-text. send()
+		res.type('txt').send('Not found');
+	})
 });
 
 //Global Router
-app.post('/dashboard',router.dashboard);
-app.get('/',router.index);
-app.get('/dashboard',router.dashboard_get);
-app.get('/signout',router.signout);
-app.get('/ProfileSetting',router.ProfileSetting);
-app.get('/PasswordReset',router.PasswordReset);
+app.post('/dashboard', router.dashboard);
+app.get('/', router.index);
+app.get('/dashboard', router.dashboard_get);
+app.get('/signout', router.signout);
+app.get('/ProfileSetting', router.ProfileSetting);
+app.get('/PasswordReset', router.PasswordReset);
 
 //Student Router
-app.get('/Assets',router.Assets);
-
+app.get('/Assets', router.Assets);
+app.get('/UsuallyTest', router.UsuallyTest);
+app.get('/UsuallyTestScores', router.UsuallyTestScores);
+app.get('/WeekTest', router.WeekTest);
+app.get('/WeekTestAnswer', router.WeekTestAnswer);
+app.get('/Homework',router.Homework);
+app.get('/HomeworkResponse',router.HomeworkResponse);
+app.get('/Question',router.Question);
 //Teacher Router
 
 
@@ -39,16 +69,22 @@ app.get('/Assets',router.Assets);
 
 
 //Test Router
-app.get('/session',function(req,res){
+app.get('/session', function(req, res) {
 	req.session.logined = true;
 	res.redirect('/');
 });
 
-app.get('/cookie',function(req,res){
-	res.cookie('userdata',{name:"Mac",email:'cslag@hotmail.com.tw'},{maxAge: new Date(Date.now() + 1000), httpOnly: true});
+app.get('/cookie', function(req, res) {
+	res.cookie('userdata', {
+		name: "Mac",
+		email: 'cslag@hotmail.com.tw'
+	}, {
+		maxAge: new Date(Date.now() + 1000),
+		httpOnly: true
+	});
 	res.redirect('/bb');
 });
-app.get('/bb',function(req,res){
+app.get('/bb', function(req, res) {
 	var data = req.cookies.userdata;
 	res.send(data);
 });
--- router/router.js
+++ router/router.js
@@ -3,102 +3,125 @@ var url = require('url'),
 	colors = require('colors'),
 	encode = require('../lib/encode'),
 	staticdb = require('../lib/staticdb'),
-	users = new staticdb('fcstu','users');
+	users = new staticdb('fcstu', 'users');
 
 /**
-* Index Login Page Handle.
-* - Cookie Check
-* - Error
-* - Lose Password
-*/
-exports.index = function(req,res){
+ * Index Login Page Handle.
+ * - Cookie Check
+ * - Error
+ * - Lose Password
+ */
+exports.index = function(req, res) {
 	/** found get method*/
-	if(req.session.logined){
+	if (req.session.logined) {
 		res.redirect('/dashboard');
 	}
 	var url_parts = url.parse(req.url, true),
 		query = url_parts.query; //query username if find
 	/** if want clean user cookie, clean*/
-	if(query.clean == "true"){
+	if (query.clean == "true") {
 		res.clearCookie('userdata');
 		res.redirect('/');
 	}
 	//如果有cookie才找username，不是本人就刪除cookie即可,資訊全部撈cookie的資料
-	if(!!req.cookies.userdata){
+	if (!!req.cookies.userdata) {
 		//if is logined
 		var cookie = req.cookies.userdata;
-		res.render('singin',{logined:true,name:cookie.name,gravatar:'http://www.gravatar.com/avatar/'+md5(cookie.email)+'?s=200'});
-	}else{
+		res.render('singin', {
+			logined: true,
+			name: cookie.name,
+			gravatar: 'http://www.gravatar.com/avatar/' + md5(cookie.email) + '?s=200'
+		});
+	}
+	else {
 		//if never login
-		res.render('singin',{logined:false});	
+		res.render('singin', {
+			logined: false
+		});
 	}
 };
 
 /**
-* Handle Login User Dashboard render
-*/
-exports.dashboard_get = function(req,res){
-	if(!!req.session.logined){
-		res.render('dashboard/index',{});
-	}else{
+ * Handle Login User Dashboard render
+ */
+exports.dashboard_get = function(req, res) {
+	if (!!req.session.logined) {
+		res.render('dashboard/index', {});
+	}
+	else {
 		res.redirect('/');
 	}
 };
 
 /**
-* Handle Dashboard router.
-* **checkLogin
-* **page render
-* 
-*/
-exports.dashboard = function(req,res){
+ * Handle Dashboard router.
+ * **checkLogin
+ * **page render
+ *
+ */
+exports.dashboard = function(req, res) {
 	//first check cookie "username". if no, check form "username" and "password". and reading
-	checkLogin(req,res,function(stats){
-		if(stats){
+	checkLogin(req, res, function(stats) {
+		if (stats) {
 			res.redirect('/dashboard');
-		}else{
+		}
+		else {
 			res.redirect('/?error=1');
 		}
 	});
 };
 
 /**
-* checkLogin function 
-* @param {String} email_
-* @param {String} password
-* @param {Object} request
-* @param {Object} response
-* @callback {boolean} if login(true)
-*/
-function checkLogin(request,response,callback){
+ * checkLogin function
+ * @param {String} email_
+ * @param {String} password
+ * @param {Object} request
+ * @param {Object} response
+ * @callback {boolean} if login(true)
+ */
+function checkLogin(request, response, callback) {
 	//check all - email and password
-	if(!!request.session.logined){
+	if (!!request.session.logined) {
 		callback(true);
-	}else{
+	}
+	else {
 		var email = request.body.email || request.cookies.userdata.email || false,
 			password = request.body.password;
-		if(email && !!password){
-			users.findOne({"email":email.toLowerCase()},function(data){
-				if(data.password == md5(password)){
+		if (email && !!password) {
+			users.findOne({
+				"email": email.toLowerCase()
+			}, function(data) {
+				if (data.password == md5(password)) {
 					request.session.logined = true;
-					request.session.user = {email:email,identity:data.identity};
-					response.cookie('userdata',{name:"Mac",email:'cslag@hotmail.com.tw'},{maxAge: new Date(Date.now() + 1000), httpOnly: true});
+					request.session.user = {
+						email: email,
+						identity: data.identity
+					};
+					response.cookie('userdata', {
+						name: "Mac",
+						email: 'cslag@hotmail.com.tw'
+					}, {
+						maxAge: new Date(Date.now() + 1000),
+						httpOnly: true
+					});
 					callback(true);
-				}else{
+				}
+				else {
 					callback(false);
 				}
 			});
-		}else{
+		}
+		else {
 			callback(false);
 		}
 	}
 }
 
 /**
-* Logout
-* clean all session and redirect to index(singin page)
-*/
-exports.signout = function(req,res){
+ * Logout
+ * clean all session and redirect to index(singin page)
+ */
+exports.signout = function(req, res) {
 	//res.clearCookie('userdata');
 	req.session.logined = null;
 	req.session.user = null;
@@ -107,44 +130,75 @@ exports.signout = function(req,res){
 
 /**
  * isLogin
-*/
-function isLogin(req,res){
-	if(!!req.session.logined){
-		return true;	
-	}else{
-		res.redirect('/');
+ */
+function isLogin(req, res) {
+		if (!!req.session.logined) {
+			return true;
+		}
+		else {
+			res.redirect('/');
+		}
+		//清除cookie也要重新登入, 重寫isLogin的程式碼, session也是
 	}
-}
-/**
-* is Student or Teacher, check Login
-*/
-function invalts(req,res){
-	if(req.session.user.identity == "student"){
+	/**
+	 * is Student or Teacher, check Login
+	 */
+function invalts(req, res) {
+	if (req.session.user.identity == "student") {
 		return "student";
-	}else if(req.session.user.identity == "teacher"){
+	}
+	else if (req.session.user.identity == "teacher") {
 		return "teacher";
-	}else{
+	}
+	else {
 		res.redirect('/');
 	}
 };
 
 /**
-* Student All Page
-*/
-exports.ProfileSetting = function(req,res){
-	isLogin(req,res);
-	res.render('dashboard/management/ProfileSetting',{});
+ * Student All Page
+ */
+exports.ProfileSetting = function(req, res) {
+	isLogin(req, res);
+	res.render('dashboard/management/ProfileSetting', {});
 };
-exports.PasswordReset = function(req,res){
-	isLogin(req,res);
-	res.render('dashboard/management/PasswordReset',{});
+exports.PasswordReset = function(req, res) {
+	isLogin(req, res);
+	res.render('dashboard/management/PasswordReset', {});
 };
-exports.Assets = function(req,res){
-	isLogin(req,res);
-	res.render('dashboard/management/Assets',{});
+exports.Assets = function(req, res) {
+	isLogin(req, res);
+	res.render('dashboard/management/Assets', {});
 };
-
+exports.UsuallyTest = function(req, res) {
+	isLogin(req, res);
+	res.render('dashboard/Student/UsuallyTest', {});
+};
+exports.UsuallyTestScores = function(req, res) {
+	isLogin(req, res);
+	res.render('dashboard/Student/UsuallyTestScores', {});
+};
+exports.WeekTest = function(req, res) {
+	isLogin(req, res);
+	res.render('dashboard/Student/WeekTest', {});
+};
+exports.WeekTestAnswer = function(req, res) {
+	isLogin(req, res);
+	res.render('dashboard/management/WeekTestAnswer', {});
+};
+exports.Homework = function(req,res){
+	isLogin(req,res);
+	res.render('dashboard/Student/Homework',{});
+};
+exports.HomeworkResponse = function(req,res){
+	isLogin(req,res);
+	res.render('dashboard/Student/HomeworkResponse',{});
+};
+exports.Question = function(req,res){
+	isLogin(req,res);
+	res.render('dashboard/management/Question',{});
+};
 
 /**
  * Teacher All Page
-*/
+ */
