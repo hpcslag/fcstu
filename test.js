67af22d0939503c6b6fd06fd0728491ff88498dc
Mac Taylor <cslag@hotmail.com.tw>
2015-01-09 05:10:10 +0000

    class database added

--- test.js
+++ test.js
@@ -2,19 +2,21 @@ var sd = require('./lib/staticdb');
 var app = new sd('fcstu','users');
 var md5 = require('./lib/hash');
 
-
+var Class = new sd('fcstu','class');
 //新增學生資料
 //app.insert({"name":"Mac","firstname":"Taylor","email":"cslag@hotmail.com.tw",'StuID':'0215161078','class':'五功二甲','password':'e10adc3949ba59abbe56e057f20f883e','identity':'student'});
 
 //新增老師資料
 //app.insert({"name":"Keyboard","Lee":"Taylor","email":"cslag@hotmail.com.tw",'password':'e10adc3949ba59abbe56e057f20f883e','identity':'teacher'});
 
-//登入查詢資料
+//新增班課資料
+Class.insert({});
 
+//登入測試資料
 var email = "keyboard@gmail.com";
 var password = "123456";
 
-app.findOne({"email":email},function(data){
+/*app.findOne({"email":email},function(data){
 	if(!!data){
 	    if(data.password == md5(password)){
 	        console.log("ok ,"+data.identity);
@@ -25,4 +27,4 @@ app.findOne({"email":email},function(data){
 	    console.log("username or password is fail!!");
 	}
 //	if(data.password == md5(password));
-});
+});*/
